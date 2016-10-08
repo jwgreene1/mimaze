20ee31ee24d82bcafe359c0f493d97f8e213d187
Josh Greene <jwgreene1@gmail.com>
2016-10-07 22:41:15 -0400

    next

--- draw/tunnel.py
+++ draw/tunnel.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
 
-#www.stuffaboutcode.com
-#Raspberry Pi, Minecraft Analogue Clock
-
-# mcpipy.com retrieved from URL below, written by stuffaboutcode
-# http://www.stuffaboutcode.com/2013/02/raspberry-pi-minecraft-analogue-clock.html
-
-#import the minecraft.py module from the minecraft directory
 import mcpi.minecraft as minecraft
 import mcpi.block as block
-#import time, so delays can be used
 import time
-#import datetime, to get the time!
 import datetime
-#import math so we can use cos and sin
 import math
 
 
 #mid point circle algorithm
-def drawCircle(mc, x0, y0, z, radius, blockType):
+def createTunnel(mc, x0, y0, z, radius, blockType, torch):
+
     blockType2 = block.AIR
-    blockType3 = block.GRASS
+    blockType3 = block.AIR
     f = 1 - radius
     ddf_x = 1
     ddf_y = -2 * radius
@@ -31,23 +22,34 @@ def drawCircle(mc, x0, y0, z, radius, blockType):
     sideSlots = round(radius * .25)
     slotFilled = dict()
 
-    print "Radius:",radius,", sideSlots:",sideSlots
+    #print "Creating Tunnel, Radius:",radius,", sideSlots:",sideSlots,", torch: ",torch
+    time.sleep(sleep)
 
-    print "Set the top block"
+    #print "Set the top block"
     mc.setBlock(x0, y0 + radius, z, blockType)
     time.sleep(sleep)
-    print "Set the bottom block"
+    #print "Set the bottom block"
     mc.setBlock(x0, y0 - radius, z, blockType)
     time.sleep(sleep)
-    print "Set the left block"
+    #print "Set the left block"
     mc.setBlock(x0 + radius, y0, z, blockType)
+    if(torch):
+        #print "Setting a torch, x:",x0 + radius,", y:",y0,", z:",z
+        mc.setBlock(x0 + radius + 1, y0, z, blockType)
+        mc.setBlock(x0 + radius, y0, z, block.TORCH, 1)
+        print "Left Torch block data: ", mc.getBlockWithData(x0 + radius , y0, z)
     time.sleep(sleep)
-    print "Set the right block"
+    #print "Set the right block"
     mc.setBlock(x0 - radius, y0, z, blockType)
+    if(torch):
+        #print "Setting a torch, x:",x0 - radius,", y:",y0,", z:",z
+        mc.setBlock(x0 - radius - 1, y0, z, blockType)
+        mc.setBlock(x0 - radius, y0, z, block.TORCH, 2)
+        print "Right Torch block data: ", mc.getBlockWithData(x0 - radius , y0, z)
     time.sleep(sleep)
 
     # Fill the center line
-    print "Fill Circle Center line verticle"
+    #print "Fill Circle Center line verticle"
     y1 = y0 - y + 1
     y2 = y0 + y
     while y1 < y2:
@@ -67,21 +69,20 @@ def drawCircle(mc, x0, y0, z, radius, blockType):
         f += ddf_x   
 
         counter += 1
-        print "x0:",x0,",y0:",y0,",x:",x,",y:",y,",f:",f,",ddf_x:",ddf_x,",ddf_y",ddf_y
 
-        print "Set the blocks to the right and left of the top blocks"
+        #print "Set the blocks to the right and left of the top blocks"
         mc.setBlock(x0 + x, y0 + y, z, blockType)
         time.sleep(sleep)
         mc.setBlock(x0 - x, y0 + y, z, blockType)
         time.sleep(sleep)
 
-        print "Set the blocks to the right and left of the bottom blocks"
+        #print "Set the blocks to the right and left of the bottom blocks"
         mc.setBlock(x0 + x, y0 - y, z, blockType)
         time.sleep(sleep)
         mc.setBlock(x0 - x, y0 - y, z, blockType)
         time.sleep(sleep)
         if(x < radius):
-            print "Fill Circle Loop 2"
+            #print "Fill Circle Loop 2"
             y1 = y0 - y + 1
             y2 = y0 + y
             while y1 < y2:
@@ -90,25 +91,26 @@ def drawCircle(mc, x0, y0, z, radius, blockType):
                 y1 += 1
                 time.sleep(sleep2)
 
-        print "Set the blocks above the side blocks"
+        #print "Set the blocks above the side blocks"
         mc.setBlock(x0 + y, y0 + x, z, blockType)
         time.sleep(sleep)
         mc.setBlock(x0 - y, y0 + x, z, blockType)
         time.sleep(sleep)
 
-        print "Set the blocks below the side blocks"
+        #print "Set the blocks below the side blocks"
         mc.setBlock(x0 + y, y0 - x, z, blockType)
         time.sleep(sleep)
         mc.setBlock(x0 - y, y0 - x, z, blockType)
         time.sleep(sleep)
-        print "y:",y
+        #print "y:",y
         if(radius > y > (radius - sideSlots)):
             if((x0 + y) in slotFilled):
-                print "slot:",(x0 + y)," is filled already, skip"
+                #print "slot:",(x0 + y)," is filled already, skip"
+                a = 1
             else:
                 y1 = y0 - x + 1
                 y2 = y0 + x
-                print "Fill the side slot blocks"
+                #print "Fill the side slot blocks"
                 while y1 < y2:
                     mc.setBlock(x0 + y, y1, z, blockType3)
                     mc.setBlock(x0 - y, y1, z, blockType3)
@@ -175,7 +177,7 @@ def drawClock(mc, clockCentre, radius, time):
     
     blockType = block.DIAMOND_BLOCK
     #draw the circle
-    drawCircle(mc, clockCentre.x, clockCentre.y, clockCentre.z, radius, blockType)
+    createTunnel(mc, clockCentre.x, clockCentre.y, clockCentre.z, radius, blockType)
 
     #draw hour hand
     #drawHourHand(mc, clockCentre, time.hour, time.minute, block.DIRT)
@@ -186,27 +188,6 @@ def drawClock(mc, clockCentre, radius, time):
     #draw second hand
     #drawSecondHand(mc, clockCentre, time.second, block.WOOD_PLANKS)
 
-#function to update the time on the clock
-def updateTime(mc, clockCentre, lastTime, time):
-    #draw hour and minute hand
-    if (lastTime.minute != time.minute):
-        #clear hour hand
-        drawHourHand(mc, clockCentre, lastTime.hour, lastTime.minute, block.AIR)
-        #new hour hand
-        drawHourHand(mc, clockCentre, time.hour, time.minute, block.DIRT)
-        
-        #clear hand
-        drawMinuteHand(mc, clockCentre, lastTime.minute, block.AIR)
-        #new hand
-        drawMinuteHand(mc, clockCentre, time.minute, block.STONE)
-
-    #draw second hand
-    if (lastTime.second != time.second):
-        #clear hand
-        drawSecondHand(mc, clockCentre, lastTime.second, block.AIR)
-        #new hand
-        drawSecondHand(mc, clockCentre, time.second, block.WOOD_PLANKS)
-
 if __name__ == "__main__":
 
     mc = minecraft.Minecraft.create()
@@ -215,19 +196,29 @@ if __name__ == "__main__":
     position = mc.player.getPos()
 
     # Now put the circle 10 blocks ahead on the z axis, y + radius
-    radius = 2
+    radius = 3
     x = position.x
-    y = position.y + radius + 1
-    z = position.z + 10
+    y = position.y + radius - 1
+    z = position.z + 1
 
-    while(z < 40):
-        clockCentre = minecraft.Vec3(x, y, z)
+    mc.postToChat("Creating a tunnel, center is: x=%s y=%s z=%s" % (int(x), int(y), int(z)))
+    print "Creating a tunnel, center is: x=%s y=%s z=%s" % (int(x), int(y), int(z))
 
-        #Post a message to the minecraft chat window 
-        mc.postToChat("Drawing a circle, center is: x=%s y=%s z=%s" % (int(x), int(y), int(z)))
-        print "Drawing a circle, center is: x=%s y=%s z=%s" % (int(x), int(y), int(z))
+    length = 0
+
+    while(length < 50):
+
+        length += 1
+        torch = 0
+
+        if(int(round(z)) % 5 == 0):
+            torch = 1
+
+        tunnelCenter = minecraft.Vec3(x, y, z)
+
+        #z -= 1 
         z += 1 
-        radius += 1 
+        #radius += 1 
 
-        lastTime = 0
-        drawClock(mc, clockCentre, radius, lastTime)
+        blockType = block.OBSIDIAN
+        createTunnel(mc, tunnelCenter.x, tunnelCenter.y, tunnelCenter.z, radius, blockType, torch)
