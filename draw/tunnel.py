a5fe900f2f8eed86d1be1784580a2f270bd1a56d
Greene <GREENEJ3@legal.regn.net>
2016-10-10 21:29:00 +0100

    more testing

--- draw/tunnel.py
+++ draw/tunnel.py
@@ -8,7 +8,7 @@ import math
 
 
 #mid point circle algorithm
-def createTunnel(mc, x0, y0, z, radius, blockType, torch):
+def createTunnelSegment(mc, x0, y0, z, radius, blockType, torch):
 
     blockType2 = block.AIR
     blockType3 = block.AIR
@@ -120,82 +120,21 @@ def createTunnel(mc, x0, y0, z, radius, blockType, torch):
                 # Set the slot hash so you don't fill this slot again
                 slotFilled[(x0 +y)] = 1
 
-#Brensenham line algorithm
-def drawLine(mc, x, y, z, x2, y2, blockType):
-    steep = 0
-    coords = []
-    dx = abs(x2 - x)
-    if (x2 - x) > 0: sx = 1
-    else: sx = -1
-    dy = abs(y2 - y)
-    if (y2 - y) > 0: sy = 1
-    else: sy = -1
-    if dy > dx:
-        steep = 1 
-        x,y = y,x
-        dx,dy = dy,dx
-        sx,sy = sy,sx
-    d = (2 * dy) - dx
-    for i in range(0,dx):
-        if steep: mc.setBlock(y, x, z, blockType)
-        else: mc.setBlock(x, y, z, blockType)
-        while d >= 0:
-            y = y + sy
-            d = d - (2 * dx)
-        x = x + sx
-        d = d + (2 * dy)
-    mc.setBlock(x2, y2, z, blockType)
-
 #find point on circle
 def findPointOnCircle(cx, cy, radius, angle):
     x = cx + math.sin(math.radians(angle)) * radius
     y = cy + math.cos(math.radians(angle)) * radius
     return((int(x + 0.5),int(y + 0.5)))
 
-def getAngleForHand(positionOnClock):
-    angle = 360 * (positionOnClock / 60.0)
-    return angle
-
-def drawHourHand(mc, clockCentre, hours, minutes, blockType):
-    if (hours > 11): hours = hours - 12
-    angle = getAngleForHand(int((hours * 5) + (minutes * (5.0/60.0))))
-    hourHandEnd = findPointOnCircle(clockCentre.x, clockCentre.y, 10.0, angle)
-    drawLine(mc, clockCentre.x, clockCentre.y, clockCentre.z - 1, hourHandEnd[0], hourHandEnd[1], blockType)
-
-def drawMinuteHand(mc, clockCentre, minutes, blockType):
-    angle = getAngleForHand(minutes)
-    minuteHandEnd = findPointOnCircle(clockCentre.x, clockCentre.y, 18.0, angle)
-    drawLine(mc, clockCentre.x, clockCentre.y, clockCentre.z, minuteHandEnd[0], minuteHandEnd[1], blockType)
-
-def drawSecondHand(mc, clockCentre, seconds, blockType):
-    angle = getAngleForHand(seconds)
-    secondHandEnd = findPointOnCircle(clockCentre.x, clockCentre.y, 20.0, angle)
-    drawLine(mc, clockCentre.x, clockCentre.y, clockCentre.z + 1, secondHandEnd[0], secondHandEnd[1], blockType)
-
-#function to draw the clock
-def drawClock(mc, clockCentre, radius, time):
-    
-    blockType = block.DIAMOND_BLOCK
-    #draw the circle
-    createTunnel(mc, clockCentre.x, clockCentre.y, clockCentre.z, radius, blockType)
-
-    #draw hour hand
-    #drawHourHand(mc, clockCentre, time.hour, time.minute, block.DIRT)
-    
-    #draw minute hand
-    #drawMinuteHand(mc, clockCentre, time.minute, block.STONE)
-
-    #draw second hand
-    #drawSecondHand(mc, clockCentre, time.second, block.WOOD_PLANKS)
-
-if __name__ == "__main__":
+# Create a tunnel
+def createTunnel():
 
     mc = minecraft.Minecraft.create()
 
     # Find your position
     position = mc.player.getPos()
 
-    # Now put the circle 10 blocks ahead on the z axis, y + radius
+    # Now put the tunnel 10 blocks ahead on the z axis, y + radius
     radius = 3
     x = position.x
     y = position.y + radius - 1
@@ -221,4 +160,8 @@ if __name__ == "__main__":
         #radius += 1 
 
         blockType = block.OBSIDIAN
-        createTunnel(mc, tunnelCenter.x, tunnelCenter.y, tunnelCenter.z, radius, blockType, torch)
+        createTunnelSegment(mc, tunnelCenter.x, tunnelCenter.y, tunnelCenter.z, radius, blockType, torch)
+
+
+if __name__ == "__main__":
+    sys.exit(createTunnel())
--- test/WallTests.py
+++ test/WallTests.py
@@ -4,16 +4,16 @@ import unittest
 
 class TestWalls(unittest.TestCase):
 
-    def test_N_facingWall(self):
+    def test_N_facing_wall(self):
         self.assertEqual('foo'.upper(), 'FO')
 
-    def test_S_facingWall(self):
+    def test_S_facing_wall(self):
         self.assertEqual('foo'.upper(), 'FO')
 
-    def test_E_facingWall(self):
+    def test_E_facing_wall(self):
         self.assertEqual('foo'.upper(), 'FO')
 
-    def test_W_facingWall(self):
+    def test_W_facing_wall(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
