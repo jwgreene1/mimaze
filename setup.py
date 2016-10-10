324b12d2306e44a9bb444782f977058fbad78897
Greene <GREENEJ3@legal.regn.net>
2016-10-10 19:42:18 +0100

    housekeeping

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mimaze',
+      version='0.1',
+      description='just a testbed for learning mcpipy',
+      url='https://github.com/jwgreene1/mimaze.git',
+      author='Josh Greene',
+      author_email='jwgreene1@gmail.com',
+      license='None',
+      packages=['mimaze'],
+      zip_safe=False)
